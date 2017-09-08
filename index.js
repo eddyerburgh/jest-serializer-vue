09127fbceaacdd4bc7dfe5627a027dc3166774b7
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-09-08 09:16:59 +0100

    initial commit 🙌

---
+++ index.js
@@ -0,0 +1,11 @@
+const beautify = require('js-beautify').html;
+
+module.exports = {
+  test(object) {
+    return typeof object === 'string' && object[0] === '<';
+  },
+  print(val) {
+    const removedServerRenderedText = val.replace(/ data-server-rendered="true"/, '')
+    return beautify(removedServerRenderedText, {});
+  },
+};
---
+++ test/List.spec.js
@@ -0,0 +1,17 @@
+import { shallow } from 'vue-test-utils'
+import List from './components/List.vue'
+import { createRenderer } from 'vue-server-renderer'
+
+describe('List.vue', () => {
+  it('hasn\'t changed snapshot', () => {
+    const renderer = createRenderer()
+    const wrapper = shallow(List, {
+      propsData: {
+        items: ['one', 'two', 'three']
+      }
+    })
+    renderer.renderToString(wrapper.vm, (err, str) => {
+      expect(str).toMatchSnapshot()
+    })
+  })
+})
